01c47f3f6f796e1fb0c034906b35a8f0501dca2c
korok <leafkorokleaf@gmail.com>
2023-11-11 19:23:26 -0500

    created basic review form

---
+++ newReview.js
@@ -0,0 +1,3 @@
+// at bottom of review section there is a button that says submit
+// Checks if user is signed in
+// Submit button sends review to database and then calls loadReview function to refresh reviews
