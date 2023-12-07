c90c45fc939a1e32deee0a4c751ff61c7c65bb24
korok <leafkorokleaf@gmail.com>
2023-12-06 22:37:03 -0500

    added regular expression form validation to signup

--- register.js
+++ register.js
@@ -1,25 +1,41 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    function validateForm(){
-
-
-
-
-        if (ul.hasChildNodes()){
-            formErrors.className = ""
+    function validateForm(event){
+        const usernameEx = /\w{3,}/
+        const passSize = /[a-zA-Z0-9]{5,}/
+        const username = document.getElementById("username")
+        const password = document.getElementById("password")
+        const confirmPass = document.getElementById("cpassword")
+        
+        if (!usernameEx.test(username.value)){
+            let message = document.createElement("li")
+            message.textContent = "Name must be atleast 3 characters!"
+            ul.append(message)
         }
-        else {
-            
-            
+        if (!passSize.test(password.value)){
+            let message = document.createElement("li")
+            message.textContent = "Password must be at least 5 characters!"
+            ul.append(message)
+        }
+        if (password.value != confirmPass.value){
+            let message = document.createElement("li")
+            message.textContent = "Passwords must match!"
+            ul.append(message)
+        }
+        if (ul.hasChildNodes()){
+            /* prevent form from being submitted if there are error messages*/
+            event.preventDefault()
         }
     }
-    const box = document.querySelector(".box")
+    const signup = document.querySelector("#signin")
     
-    box.addEventListener("click", (event)=>{
+    signup.addEventListener("submit", (event)=>{
         formErrors.innerHTML = null
         formErrors.append(ul)
-        validateForm()
-        event.preventDefault()
+        document.querySelector("ul").innerHTML = null
+        validateForm(event)
+        
     })
+    
     const formErrors = document.getElementById("formErrors")
     const ul = document.createElement("ul")
 })
