b9b2d178a293a2e744081aba244bdeaff5cfd792
korok <leafkorokleaf@gmail.com>
2023-12-12 09:22:03 -0500

    added logout redirect conditional

--- logout.php
+++ logout.php
@@ -1,10 +1,15 @@
 <?php
 session_start();
-// re
+$_SESSION = [];
+// try to redirect user to last page
 if(isset($_SERVER['HTTP_REFERER'])){
     $lastpage = $_SERVER['HTTP_REFERER'];
+    header("Location: $lastpage");
+    exit();
 }
-$_SESSION = [];
-header("Location: $lastpage");
-exit();
+// if it fails then send to home instead
+else{
+    header("Location: home.php");
+    exit();
+}
 ?>
